234d027b98a02d72d41f31799ecbfa743b30374b
Chris Ware <c.p.ware@hotmail.com>
2022-11-12 21:21:19 -0700

    Nav2 Done

--- components/nav2/Nav2.js
+++ components/nav2/Nav2.js
@@ -1,8 +1,39 @@
-import styles from './Nav2.module.css'
+import Link from "next/link";
+
+import styles from "./Nav2.module.css";
 
 const Nav2 = () => {
+
   return (
-    <div>Nav2</div>
-  )
-}
-export default Nav2
+    <nav className={styles.navbar}>
+      <ul className={styles.navmenu}>
+        <li className={styles.navItem}>
+          <Link href='/blue' passHref>
+            <a>Blue</a>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href='/blue' passHref>
+            <a>Green</a>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href='/blue' passHref>
+            <a>Black</a>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href='/blue' passHref>
+            <a>Gold</a>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href='/blue' passHref>
+            <a>Specialties</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Nav2;
