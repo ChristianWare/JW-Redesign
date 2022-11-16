6a11df223623a55a4e2b25dacc5b0e6ca0831b18
Chris Ware <c.p.ware@hotmail.com>
2022-11-15 18:55:33 -0700

    Order History Start

--- pages/order-history.js
+++ pages/order-history.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { useEffect, useReducer } from "react";
 import Layout from "../components/layout/Layout";
 import { getError } from "../utils/error";
+import styles from "../styles/OrderHistory.module.css";
+
 
 function reducer(state, action) {
   switch (action.type) {
@@ -39,51 +41,53 @@ function OrderHistoryScreen() {
 
   return (
     <Layout title='Order History'>
-      <h1 className='mb-4 text-xl'>Order History</h1>
-      {loading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <div className='alert-error'>{error}</div>
-      ) : (
-        <div className='overflow-x-auto'>
-          <table className='min-w-full'>
-            <thead className='border-b'>
-              <tr>
-                <th className='px-5 text-left'>ID</th>
-                <th className='px-5 text-left'>DATE</th>
-                <th className='px-5 text-left'>TOTAL</th>
-                <th className='px-5 text-left'>PAID</th>
-                <th className='px-5 text-left'>DELIVERED</th>
-                <th className='px-5 text-left'>ACTION</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order._id} className='border-b'>
-                  <td className='p-5'>{order._id.substring(20, 24)}</td>
-                  <td className='p-5'>{order.createdAt.substring(0, 10)}</td>
-                  <td className='p-5'>{order.totalPrice}</td>
-                  <td className='p-5'>
-                    {order.isPaid
-                      ? `${order.paidAt.substring(0, 10)}`
-                      : "not paid"}
-                  </td>
-                  <td className='p-5'>
-                    {order.isDelivered
-                      ? `${order.deliveredAt.substring(0, 10)}`
-                      : "not delivered"}
-                  </td>
-                  <td className='p-5'>
-                    <Link href={`/order/${order._id}`} passHref>
-                      <a>Details</a>
-                    </Link>
-                  </td>
+      <div className={styles.container}>
+        <h1>Order History</h1>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div className='alert-error'>{error}</div>
+        ) : (
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>DATE</th>
+                  <th>TOTAL</th>
+                  <th>PAID</th>
+                  <th>DELIVERED</th>
+                  <th>ACTION</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+              </thead>
+              <tbody>
+                {orders.map((order) => (
+                  <tr key={order._id} className='border-b'>
+                    <td>{order._id.substring(20, 24)}</td>
+                    <td>{order.createdAt.substring(0, 10)}</td>
+                    <td>{order.totalPrice}</td>
+                    <td>
+                      {order.isPaid
+                        ? `${order.paidAt.substring(0, 10)}`
+                        : "not paid"}
+                    </td>
+                    <td className='p-5'>
+                      {order.isDelivered
+                        ? `${order.deliveredAt.substring(0, 10)}`
+                        : "not delivered"}
+                    </td>
+                    <td className='p-5'>
+                      <Link href={`/order/${order._id}`} passHref>
+                        <a>Details</a>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </Layout>
   );
 }
