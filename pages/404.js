26339dcd97f6f4606930a0da5f831669edf1d038
Chris Ware <c.p.ware@hotmail.com>
2022-11-16 15:17:32 -0700

    404 Start

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout/Layout";
+import styles from "../styles/NotFoundPage.module.css";
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+    <h1>404 Page Not Found</h1>
+    </Layout>
+  )
+}
+export default NotFoundPage;
