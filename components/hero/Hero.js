b5e005b49e9e2a7487cb9bbfe76dba25a2070908
Chris Ware <c.p.ware@hotmail.com>
2022-11-17 00:46:33 -0700

    Hero Grid Template Columns

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -2,32 +2,27 @@ import Button from "../button/Button";
 import styles from "./Hero.module.css";
 import ImageSlider from "../imageSlider/ImageSlider";
 import { SlideData } from "../slideData/SlideData";
-import PageIntro from "../pageIntro/PageIntro";
 
 const hero = () => {
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.top}>
-            <div className={styles.topLeft}>
-              <h1>The greatness of Johnnie</h1>
-              <div className={styles.btnContainer}>
-                <Button text='View Categlog' btnType="orange" iconColor="whiteIcon" href='/products' />
-              </div>
-            </div>
-            <div className={styles.topRight}>
-              <ImageSlider slides={SlideData} />
-            </div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.topLeft}>
+          <h1>The greatness of Johnnie</h1>
+          <div className={styles.btnContainer}>
+            <Button
+              text='View Categlog'
+              btnType='orange'
+              iconColor='whiteIcon'
+              href='/products'
+            />
           </div>
         </div>
-      </div>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.bottom}></div>
+        <div className={styles.topRight}>
+          <ImageSlider slides={SlideData} />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default hero;
