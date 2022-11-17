60d076790db86e31c5cb69b4c40bf2874cc2d969
Chris Ware <c.p.ware@hotmail.com>
2022-11-17 01:17:34 -0700

    Black Label

---
+++ components/black/BlackLabel.js
@@ -0,0 +1,44 @@
+import styles from "./BlackLabel.module.css";
+import Image from "next/image";
+import JW from "../../public/icons/jw.png";
+import { useRouter } from "next/router";
+
+const BlackLabel = () => {
+  const router = useRouter();
+
+  const onClick = () => {
+    if (router.pathname === "/") {
+      router.push("/red");
+    }
+    return;
+  };
+
+  return (
+    <div
+      className={
+        router.pathname === "/"
+          ? styles.container + " " + styles.hover
+          : styles.container
+      }
+      onClick={onClick}
+    >
+      <div className={styles.border}>
+        <div className={styles.border}>
+          <div className={styles.left}>
+            <div className={styles.textContainer}>
+              <p className={styles.jw}>JOHNNIE WALKER</p>
+              <p className={styles.rl}>BLACK LABEL</p>
+            </div>
+            <div className={styles.lowerTop}>
+              <div className={styles.imgContainer}>
+                <Image src={JW} width='50' height='83' alt='image' />
+              </div>
+              <p className={styles.scriptText}>Blended Scotch Whisky</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default BlackLabel;
--- pages/black.js
+++ pages/black.js
@@ -8,6 +8,7 @@ import db from "../utils/db";
 import Product from "../models/Product";
 import styles from "../styles/Blue.module.css";
 import ProductItem from "../components/productItem/ProductItem";
+import BlackLabel from "../components/black/BlackLabel";
 
 const BlackPage = ({ black }) => {
   const { state, dispatch } = useContext(Store);
@@ -27,6 +28,9 @@ const BlackPage = ({ black }) => {
   };
   return (
     <Layout title='black'>
+      <div className={styles.labelContainer}>
+        <BlackLabel />
+      </div>
       <PageIntro
         title='The Greatness of Black'
         text='The original Walker family blend, handcrafted from as many as 40 of the finest Scotch whiskies aged a minimum of 12 years, for a smooth and robust blend. Rich smoky malt, peat and sherry fruit character deliver a satisfyingly complex flavor on the long, lingering finish.'
